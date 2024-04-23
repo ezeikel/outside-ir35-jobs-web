69474e11419daeb74da30e433d293df5647c2070
Ezeikel Pemberton <hi@ezeikel.dev>
2024-04-23 19:27:54 +0100

    chore: updated next config.

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,18 @@ import { withSentryConfig } from '@sentry/nextjs';
 import { withPlausibleProxy } from 'next-plausible';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@react-pdf/renderer'],
+  },
+};
 
-// Sentry configuration options
+// sentry configuration options
 const sentryOptions = {
   silent: true,
   org: 'ezeikel',
   project: 'outside-ir35-jobs-web',
+  authToken: process.env.SENTRY_AUTH_TOKEN,
   widenClientFileUpload: true,
   transpileClientSDK: true,
   tunnelRoute: '/monitoring',
